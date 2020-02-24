b96a7aa1fa6357baed2ec2dd9f75eccdb328a3f3
palehorse1985216 <robbie1985216@gmail.com>
2020-02-24 14:29:56 +0800

    設定 Session domain

--- bootstrap.php
+++ bootstrap.php
@@ -23,6 +23,7 @@ switch (Config::get('project.ENV')) {
 date_default_timezone_set("Asia/Taipei");
 mb_internal_encoding('UTF-8');
 ini_set('session.cookie_path', sprintf('/%s/', Config::get('project.NAME')));
+ini_set('session.cookie_domain', $_SERVER['HTTP_HOST']);
 ini_set('session.cookie_lifetime', 3600); 
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
