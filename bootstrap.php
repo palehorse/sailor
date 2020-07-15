313a14b2583327d19ee87100de59ffba2d4e7401
palehorse1985216 <robbie1985216@gmail.com>
2020-07-15 14:48:01 +0800

    調整啟動程式

--- bootstrap.php
+++ bootstrap.php
@@ -2,9 +2,11 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Sailor\Core\Builders\ExtensionBuilder;
+use Sailor\Core\Builders\HandlerBuilder;
 use Sailor\Core\Config;
-
-Config::init();
+use Sailor\Core\Router;
+use Slim\Views\Twig;
 
 /** Environment Setup */
 switch (Config::get('project.ENV')) {
@@ -27,3 +29,8 @@ ini_set('session.name', !empty(Config::get('session.NAME')) ? Config::get('sessi
 ini_set('session.cookie_path', sprintf('/%s/', !empty(Config::get('session.PATH')) ? Config::get('session.PATH') : Config::get('project.NAME')));
 ini_set('session.cookie_domain', !empty(Config::get('session.DOMAIN')) ? Config::get('session.DOMAIN') : $_SERVER['HTTP_HOST']);
 ini_set('session.cookie_lifetime', !empty(Config::get('session.LIFE_TIME')) ? Config::get('session.LIFE_TIME') : 3600); 
+
+/** Routes */
+foreach (glob(__DIR__ . '/routes/*.php') as $route) {
+    include_once $route;
+}
