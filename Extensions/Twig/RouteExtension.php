c5a0826c99aee97543888fd14c5596f58c0e8b33
palehorse1985216 <robbie1985216@gmail.com>
2021-01-03 20:32:53 +0800

    移除多餘的Extenstion、加入註解

--- Extensions/Twig/RouteExtension.php
+++ Extensions/Twig/RouteExtension.php
@@ -6,6 +6,11 @@ use \Twig\TwigFunction;
 use \Twig\Extension\AbstractExtension;
 use Sailor\Core\Router;
 
+/**
+ * 與路徑相關的Twig功能
+ * 
+ * @author palehose
+ */
 class RouteExtension extends AbstractExtension
 {
     public function getFunctions()
@@ -16,11 +21,23 @@ class RouteExtension extends AbstractExtension
         ];
     }
 
+    /**
+     * 替js、css的檔名加入時間戳記以避免瀏覽器暫存
+     * 
+     * @param string $url css或js的網址
+     * @return string 加入時間戳記後的網址
+     */
     public function version($url)
     {
         return Router::version($url);
     }
 
+    /**
+     * 根據 Route name 產生實際路徑
+     * 
+     * @param string $name Route name
+     * @return string 實際路徑
+     */
     public function pathFor($name)
     {
         return Router::pathFor($name);
